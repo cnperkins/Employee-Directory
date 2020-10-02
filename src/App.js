d8f05ab8c21a3dea3fb97c101d4704a4cf770707
chlokins <cnpolansky@gmail.com>
2020-10-02 18:48:09 -0500

    added nav

--- src/App.js
+++ src/App.js
@@ -1,3 +1,7 @@
 import React from 'react';
 import './App.css';
 import API from "./API/API";
+import Navbar from "./components/Navbar";
+import Table from "./components/Table";
+import moment from "moment";
+
