638afce4687900f9bd49029e8760c3401039f348
chlokins <cnpolansky@gmail.com>
2020-10-02 22:17:36 -0500

    deploying

--- server/server.js
+++ server/server.js
@@ -2,8 +2,6 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
-app.set("port", PORT);
-
 const publicPath = path.join(__dirname, '..', 'public');
 
 const port = process.env.PORT || 3000;
