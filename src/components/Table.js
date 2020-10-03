d3e5db8eef2c7a2046b2faaea5e0636d1b5221f5
chlokins <cnpolansky@gmail.com>
2020-10-02 21:04:49 -0500

    deploying

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,2 +1,16 @@
 import React from "react";
 
+export default function Table(props) {
+    return (
+        <tr className="text-center h5 text-secondary">
+            <td>
+                <img src={props.photo} alt="employee" />
+            </td>
+            <td className="align-middle">{props.name}</td>
+            <td className="align-middle">{props.email}</td>
+            <td className="align-middle">{props.phone}</td>
+            <td className="align-middle">{props.location}</td>
+            <td className="align-middle">{props.dob}</td>
+        </tr>
+    );
+}
